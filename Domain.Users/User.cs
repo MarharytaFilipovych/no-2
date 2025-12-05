52539ff9bf87d5ad1ef74b1cfddd581095becf8c
sofiia.churikova <schurikova@kse.org.ua>
2025-12-05 16:25:47 +0200

    update user repository

--- Domain.Users/User.cs
+++ Domain.Users/User.cs
@@ -4,4 +4,20 @@ public class User
 {
     public Guid UserId { get; init; }
     public string? Email { get; init; }
+    public DateTime? BannedUntil { get; set; }
+
+    public bool IsBanned(DateTime currentTime)
+    {
+        return BannedUntil.HasValue && currentTime < BannedUntil.Value;
+    }
+
+    public void Ban(DateTime until)
+    {
+        BannedUntil = until;
+    }
+
+    public void Unban()
+    {
+        BannedUntil = null;
+    }
 }
